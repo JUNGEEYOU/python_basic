56b6bb90aa0bbfa1e7e30b153de10fbd3833d02a
JUNGEEYOU <junge2u@naver.com>
2020-10-16 16:01:20 +0900

    conditional

---
+++ 2_Conditional/1.py
@@ -0,0 +1,14 @@
+"""
+ 1. 조건문
+"""
+# 파이썬 기타 연산자
+# - 한줄 조건문
+score = 85
+if score >= 80: result = 'success'
+else: result = 'fail'
+print(result)
+
+# - 조건부 표현식
+score = 85
+result = "success" if score >= 80 else "fail"
+print(result)
