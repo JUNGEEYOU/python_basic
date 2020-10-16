fb720228c91159973991892397c2b267ef52187f
JUNGEEYOU <junge2u@naver.com>
2020-10-16 16:17:25 +0900

    input output basic

---
+++ 4_function/1.py
@@ -0,0 +1,13 @@
+"""
+ 1. 함수
+  - 람다 표현식
+"""
+
+def add(a, b):
+    return a + b
+
+print(add(3, 7))
+
+# 람다 표현식
+print((lambda a, b: a + b)(3, 7))
+
---
+++ 5_input_output/1.py
@@ -0,0 +1,13 @@
+"""
+ 입출력 사용법
+"""
+# - 입력
+import sys
+read =  sys.stdin.readline()
+print(read)
+
+read2 =  sys.stdin.readline().split()
+print(read2)
+
+read3 =  list(map(int, sys.stdin.readline().split()))
+print(read3)
